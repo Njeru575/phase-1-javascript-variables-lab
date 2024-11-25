624170779de63041a9de7ecd40a31e5b96095319
emmanuel <emmanjeru2002@gmail.com>
2024-11-25 12:26:46 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+// Declare companyName as a constant and assign it the value "Scuber"
+const companyName = "Scuber";
+
+// Declare mostProfitableNeighborhood using let and assign it the value "Chelsea"
+let mostProfitableNeighborhood = "Chelsea";
+
+// Declare companyCeo using let and assign it the value "Susan Smith"
+let companyCeo = "Susan Smith";
